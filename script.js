2789fde61ae5a870d90e809254fdd204468e4839
Mr.Fox <nayeemkhanjim10@gmail.com>
2024-09-23 14:46:30 +0600

    footer is completed

--- script.js
+++ script.js
@@ -14,11 +14,22 @@ const navToggle = document.querySelector('.nav-toggle');
 const navList = document.querySelector('.nav-list');
 const navClose = document.querySelector('.close-toggle-icon');
 
+
 navToggle.addEventListener('click', () => {
     navList.classList.add('active');
+
 });
 
 navClose.addEventListener('click', () => {
     navList.classList.remove('active');
+});
 
-})
+
+const navLink = document.querySelectorAll('.nav-link');
+function linkAction() {
+    const navList = document.querySelector('.nav-list')
+    navList.classList.remove('active')
+
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
